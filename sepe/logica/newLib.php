33888b563688263125890f98f96a61e61b1a0626
Doctorspeppers <doctorspeppers@localhost.localdomain>
2018-12-19 18:59:22 -0200

    newLib criada

---
+++ sepe/logica/newLib.php
@@ -0,0 +1,26 @@
+<?php
+/*
+    Modelo de json para contas
+
+    {
+    "categoria" : "Eletrostática",
+    "nomeFormula": "Quantidade de Carga Elétrica",
+    "formula": " Q = ( n * e ) ",
+    "incog": ["Q", "n", "e"]
+},
+
+*/
+
+
+function abreJson(Type $var = null)
+{
+    # code...
+}
+
+function verificaEquação($equacao, $variaveis)
+{
+    $equacaoaberta = explode(" ", $equacao);
+
+}
+
+?>
